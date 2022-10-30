90ee5d3219095b02a4c0d9346906fd844310f48c
Shuvam Mondal <96789016+Shuvam-M-Astro@users.noreply.github.com>
2022-10-30 19:43:42 +0100

    Update Arsenic.sql

--- Arsenic.sql
+++ Arsenic.sql
@@ -1,25 +1,89 @@
-DROP SCHEMA accidents;
-CREATE SCHEMA accidents;
+DROP SCHEMA vals;
+CREATE SCHEMA vals;
 
-USE accidents;
+USE vals;
 
 /* -------------------------------- */
 /* Create Tables */
-CREATE TABLE accident(
-	accident_index VARCHAR(2000),
-    accident_severity double
+CREATE TABLE val(
+	STATE VARCHAR(2000),
+    mean_2011 double,
+    mean_2012 double,
+    mean_2013 double,
+    mean_2014 double,
+    mean_2015 double
 );
 
+CREATE TABLE avgSTATE(
+	STATE VARCHAR(2000),
+    mean_2011 double,
+    mean_2012 double,
+    mean_2013 double,
+    mean_2014 double,
+    mean_2015 double
+);
 /* -------------------------------- */
 /* Load Data */
-LOAD DATA LOCAL INFILE 'C:\\Annual-tabulation_Arsenic in particulate matter_2011 - Annual-tabulation_Arsenic in particulate matter_2011.csv'
-INTO TABLE accident
+LOAD DATA LOCAL INFILE 'C://Arsenic//Annual-tabulation_Arsenic in particulate matter_2011.csv'
+INTO TABLE val
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 LINES
 (@col1, @dummy, @dummy, @dummy, @dummy, @col2)
-SET accident_index=@col1, accident_severity=@col2;
+SET STATE=@col1, mean_2011=@col2;
 
+LOAD DATA LOCAL INFILE 'C://Arsenic//Annual-tabulation_Arsenic in particulate matter_2012.csv'
+INTO TABLE val
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@col1, @dummy, @dummy, @dummy, @dummy, @col2)
+SET STATE=@col1, mean_2012=@col2;
+
+LOAD DATA LOCAL INFILE 'C://Arsenic//Annual-tabulation_Arsenic in particulate matter_2013.csv'
+INTO TABLE val
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@col1, @dummy, @dummy, @dummy, @dummy, @col2)
+SET STATE=@col1, mean_2013=@col2;
+
+LOAD DATA LOCAL INFILE 'C://Arsenic//Annual-tabulation_Arsenic in particulate matter_2014.csv'
+INTO TABLE val
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@col1, @dummy, @dummy, @dummy, @dummy, @col2)
+SET STATE=@col1, mean_2014=@col2;
+
+LOAD DATA LOCAL INFILE 'C://Arsenic//Annual-tabulation_Arsenic in particulate matter_2015.csv'
+INTO TABLE val
+FIELDS TERMINATED BY ','
+ENCLOSED BY '"'
+LINES TERMINATED BY '\n'
+IGNORE 1 LINES
+(@col1, @dummy, @dummy, @dummy, @dummy, @col2)
+SET STATE=@col1, mean_2015=@col2;
+
+/*%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% */
+
+
+/*Average by states*/
+SELECT STATE 'Mean_STATE',
+AVG(mean_2011) 'Mean' 
+FROM val
+AS avg_mean_2011
+GROUP BY STATE;
+
+/*Average by states*/
+SELECT STATE 'Mean_STATE',
+AVG(mean_2012) 'Mean' 
+FROM val
+AS avg_mean_2012
+GROUP BY STATE;
 
 
